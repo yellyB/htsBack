f9163fb97145eec842d56dab485e1c33ec394383
bom\bomi <kk3p1q1@gmail.com>
2022-01-16 23:58:26 +0900

    업비트 모듈 설치

--- backend/post/views.py
+++ backend/post/views.py
@@ -4,8 +4,14 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
+import pyupbit
+
 def test():
     print('fwefwefw')
+
+    # tickers = pyupbit.get_tickers(fiat="KRW")
+    price = pyupbit.get_current_price("KRW-XRP")
+    print(price)
     return
 test()
 
